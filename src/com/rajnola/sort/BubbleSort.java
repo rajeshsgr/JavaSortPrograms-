10a72a813c6036a89516e3b30302d756dd33f3ee
Rajesh P <rajeshp@Rajeshs-MacBook-Pro.local>
2019-12-15 17:45:50 -0600

    Program to sort an array using Bubble Sort

---
+++ src/com/rajnola/sort/BubbleSort.java
@@ -0,0 +1,34 @@
+package com.rajnola.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] dataArray = { 10, -20, 20, 5, 80, 30, -10, 75, 2 };
+
+		System.out.println("Before Bubble Sort :- "+Arrays.toString(dataArray));
+		
+       // int temp = 0;  
+
+
+		for (int i = 0; i < dataArray.length; i++) {
+
+			for (int j = 1; j < (dataArray.length); j++) {
+
+				if (dataArray[j - 1] > dataArray[j]) {
+
+					int temp = dataArray[j - 1];
+					dataArray[j - 1] = dataArray[j];
+					dataArray[j] = temp;
+				}
+			}
+		}
+
+		System.out.println("\nAfter Bubble Sort :- " +Arrays.toString(dataArray));
+
+	}
+
+}
